8ba34726b1f732ce6b2cc75f00a332c6789c8773
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-01-30 18:28:47 +0900

    Update motor_p_control.py

--- bbm/motor_p_control.py
+++ bbm/motor_p_control.py
@@ -1,25 +1,22 @@
 from machine import Pin, PWM
 import time
 
-# モーターA設定
+# モーターピン設定
 AIN1 = Pin(18, Pin.OUT)
 AIN2 = Pin(17, Pin.OUT)
 PWMA = PWM(Pin(16))
 PWMA.freq(1000)
 OUTA_1 = Pin(3, Pin.IN)
 OUTB_1 = Pin(2, Pin.IN)
-
-# モーターB設定
 BIN1 = Pin(19, Pin.OUT)
 BIN2 = Pin(22, Pin.OUT)
 PWMB = PWM(Pin(28))
 PWMB.freq(1000)
 OUTA_2 = Pin(7, Pin.IN)
 OUTB_2 = Pin(6, Pin.IN)
-
-# その他設定
 STBY = Pin(9, Pin.OUT)
 
+# エンコーダーの設定
 PPR = 3  # PPR = CPR / 4
 GEAR_RATIO = 297.92
 INTERVAL = 0.1
@@ -34,7 +31,7 @@ target_rpm_A = 60
 target_rpm_B = 60
 
 # モーターAのエンコーダ処理
-def pulseCounterA(pin):
+def pulse_counter_A(pin):
     global pulse_count_A, direction_A
     if OUTB_1.value() == 1:
         direction_A = 1
@@ -43,7 +40,7 @@ def pulseCounterA(pin):
     pulse_count_A += direction_A
 
 # モーターBのエンコーダ処理
-def pulseCounterB(pin):
+def pulse_counter_B(pin):
     global pulse_count_B, direction_B
     if OUTB_2.value() == 1:
         direction_B = 1
@@ -51,28 +48,27 @@ def pulseCounterB(pin):
         direction_B = -1
     pulse_count_B += direction_B
 
-# モーターAの前進
-def forwardA(rate):
-    OUTA_1.irq(trigger=Pin.IRQ_RISING, handler=pulseCounterA)
+# 前進
+def forward(rate_A, rate_B):
+    OUTA_1.irq(trigger=Pin.IRQ_RISING, handler=pulse_counter_A)
     STBY.on()
     AIN1.on()
     AIN2.off()
-    PWMA.duty_u16(2**16 // 100 * rate)
-
-# モーターBの前進
-def forwardB(rate):
-    OUTA_2.irq(trigger=Pin.IRQ_RISING, handler=pulseCounterB)
+    PWMA.duty_u16(2**16 // 100 * rate_A)
+    
+    OUTA_2.irq(trigger=Pin.IRQ_RISING, handler=pulse_counter_B)
     STBY.on()
     BIN1.off()
     BIN2.on()
-    PWMB.duty_u16(2**16 // 100 * rate)
+    PWMB.duty_u16(2**16 // 100 * rate_B)
 
 # 停止
 def stop():
     OUTA_1.irq(handler=None)
-    OUTA_2.irq(handler=None)
     AIN1.off()
     AIN2.off()
+    
+    OUTA_2.irq(handler=None)
     BIN1.off()
     BIN2.off()
 
@@ -82,13 +78,12 @@ def calculateRPM(pulse_count, interval):
     return rpm
 
 # 制御ループ
-def moterPcontral():
+def moter_p_contral():
     global pulse_count_A, pulse_count_B, target_rpm_A, target_rpm_B
     rate_A = 0
     rate_B = 0
     while True:
-        forwardA(int(rate_A))
-        forwardB(int(rate_B))
+        forward(int(rate_A), int(rate_B))
         time.sleep(0.1)
         
         current_rpm_A = abs(calculateRPM(pulse_count_A, 0.1))
@@ -105,11 +100,9 @@ def moterPcontral():
         
         print(f"Motor A RPM: {current_rpm_A}, Motor B RPM: {current_rpm_B}")
 
-# 実行
-try:
-    moterPcontral()
-except KeyboardInterrupt:
-    stop()
-    print("Stop!!")
-
-
+if __name__ == '__main__':
+    try:
+        moter_p_contral()
+    except KeyboardInterrupt:
+        stop()
+        print("Stop!!")
