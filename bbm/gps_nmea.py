63603c45b0020da679eec3151983c03d15a5d7de
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-01-30 15:23:15 +0900

    Update gps_nmea.py

--- bbm/gps_nmea.py
+++ bbm/gps_nmea.py
@@ -2,11 +2,12 @@ from machine import UART
 
 uart0 = UART(0, baudrate=9600, tx=0, rx=1)
 
-while True:
-    if uart0.any():
-        sentence = uart0.read()
-        if sentence:
-            try:
-                print(sentence.decode('utf-8'))
-            except Exception as e:
-                print("error", e)
+if __name__ == '__main__':
+    while True:
+        if uart0.any():
+            sentence = uart0.read()
+            if sentence:
+                try:
+                    print(sentence.decode('utf-8'))
+                except Exception as e:
+                    print("error", e)
