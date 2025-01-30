8612d185483e9b509a0d66512c238067b2d979a6
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-01-30 12:50:29 +0900

    Update gps_data.py

--- bbm/gps_data.py
+++ bbm/gps_data.py
@@ -5,7 +5,7 @@ import time
 uart0 = UART(0, baudrate=9600, tx=0, rx=1)
 gps = MicropyGPS(9, 'dd')
 
-def readGPS():
+def read_gps():
     if uart0.any() > 0:
         sentence = uart0.read()
         if sentence:
@@ -16,18 +16,27 @@ def readGPS():
                 print("Error decoding sentence:", e)
 
 while True:
-    readGPS()
-    #経度、緯度
-    print(f"Latitude: {gps.latitude[0]}°, Longitude: {gps.longitude[0]}°")
+    read_gps()
+    
+    #データ取得状況
+    if gps.fix_stat > 0:
+        print("GPS信号を受信")
+        
+        #経度、緯度
+        print(f"Latitude: {gps.latitude[0]}°, Longitude: {gps.longitude[0]}°")
 
-    #PDOP
-    print(f"PDOP: {gps.pdop}")
+        #PDOP
+        print(f"PDOP: {gps.pdop}")
 
-    #測位可能衛星数
-    print(f"Satellites in view: {gps.satellites_in_view}")
+        #測位可能衛星数
+        print(f"Satellites in view: {gps.satellites_in_view}")
 
-    #信号強度
-    print("Signal Strengths (SNR):")
-    for satellite_id, data in gps.satellite_data.items():
-        elevation, azimuth, snr = data
-        print(f"  Satellite ID: {satellite_id}, SNR: {snr}")
+        #信号強度
+        print("Signal Strengths (SNR):")
+        for satellite_id, data in gps.satellite_data.items():
+            elevation, azimuth, snr = data
+            print(f"  Satellite ID: {satellite_id}, SNR: {snr}")
+
+    else:
+        print("GPS信号を取得できていません")
+        time.sleep(1)
