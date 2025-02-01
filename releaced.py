d364798682c6ae3aa046179d736e580fabb9968c
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-02-01 16:20:25 +0900

    Create releaced.py

---
+++ releaced.py
@@ -0,0 +1,62 @@
+import machine
+import time
+import math
+from bno055 import *
+i2c = machine.I2C(0, sda=machine.Pin(20), scl=machine.Pin(21))  
+
+
+bno = BNO055(i2c)
+def euler():
+    bno.iget(QUAT_DATA)
+   
+
+    w = (bno.w / 16384)
+    x = (bno.x / 16384)
+    y = (bno.y / 16384)
+    z = (bno.z / 16384)
+
+    ysqr = y * y
+
+    # roll (x-axis rotation)
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + ysqr)
+    roll = math.atan2(t0, t1);
+
+    # pitch (y-axis rotation)
+    t2 = +2.0 * (w * y - z * x)
+    if t2 > 1.0:
+        t2 = 1.0
+    
+    elif t2 < -1.0:
+        t2 = -1.0
+    pitch = math.asin(t2)
+
+    # yaw (z-axis rotation)
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (ysqr + z * z) 
+    yaw = math.atan2(t3, t4)
+
+    roll *= 180/math.pi
+    pitch *= 180/math.pi
+    yaw *= 180/math.pi
+    return roll, pitch, yaw
+
+def released():
+    j = 0
+
+    while True:
+        time.sleep(0.1)
+        roll, pitch, yaw = euler()
+
+        if abs(roll) < 45: 
+          j += 1
+        else: 
+          j = 0
+          
+        if j >= 5:
+            print("released")
+            break
+        print(f"roll:{roll}")
+    
+released()          
+        
