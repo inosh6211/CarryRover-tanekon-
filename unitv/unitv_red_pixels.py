494a01d1f44dd7d6124f7ea5d8f5be8805cd2a71
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-02-03 15:55:06 +0900

    Update unitv_red_pixels.py

--- unitv/unitv_red_pixels.py
+++ unitv/unitv_red_pixels.py
@@ -6,14 +6,17 @@ from machine import UART
 from fpioa_manager import fm
 from Maix import GPIO
 
+#　LEDの設定
 led = ws2812(8, 1)
 led.set_led(0, (0, 255, 0))
 led.display()
 
+# UARTの設定
 fm.register(34, fm.fpioa.UART1_TX, force=True)
 fm.register(35, fm.fpioa.UART1_RX, force=True)
 uart = UART(UART.UART1, 115200, 8, None, 1, timeout=1000, read_buf_len=4096)
 
+# カメラの初期設定
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.VGA)
@@ -21,7 +24,7 @@ sensor.set_auto_gain(False)
 sensor.set_auto_whitebal(False)
 sensor.skip_frames(time=2000)
 
-# 赤色のしきい値（色相・彩度・明度）
+# 赤色の閾値（LAB）
 red_threshold = (20, 130, 15, 127, 15, 127)
 
 while True:
