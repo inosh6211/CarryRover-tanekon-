a3c11f88b987da74ce79d8bccca91a436dde7288
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-02-03 15:04:50 +0900

    Create unitv_led.py

---
+++ unitv/unitv_led.py
@@ -0,0 +1,13 @@
+import time
+from modules import ws2812
+
+led = ws2812(8, 1)
+
+while True:
+    led.set_led(0, (0, 255, 0))  # 赤色
+    led.display()
+    time.sleep(0.5)
+
+    led.set_led(0, (0, 0, 0))  # 消灯
+    led.display()
+    time.sleep(0.5)
