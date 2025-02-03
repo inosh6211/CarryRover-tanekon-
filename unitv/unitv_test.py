59cd23328e54195629a93a792a8ef976686de714
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-02-03 14:21:00 +0900

    Update unitv_test.py

--- unitv/unitv_test.py
+++ unitv/unitv_test.py
@@ -2,19 +2,16 @@ import sensor
 import lcd
 import time
 
-# LCDの初期化
 lcd.init()
-
-# カメラの初期化
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.VGA)
-sensor.set_auto_gain(True)
-sensor.set_auto_whitebal(True)
-sensor.skip_frames(time=2000)  # 設定が安定するまで待機
+sensor.set_auto_gain(False)
+sensor.set_auto_whitebal(False)
+sensor.skip_frames(time=2000)
 
-# メインループ
 while True:
-    img = sensor.snapshot()  # 画像を取得
-    lcd.display(img)  # LCDに表示
-    time.sleep(0.05)
+    img = sensor.snapshot()
+    img.replace(vflip = False, hmirror = True, transpose = True)
+    lcd.display(img)
+    time.sleep(0.1)
