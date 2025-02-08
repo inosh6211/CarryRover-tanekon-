bc2ab96fdcb01ea7ed939f387161943678ed9be8
Tatsuki-300 <7524040@ed.tus.ac.jp>
2025-02-08 15:39:35 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import bluetooth
+from ble_simple_peripheral import BLESimplePeripheral
+import time
+
+ble = bluetooth.BLE()
+p = BLESimplePeripheral(ble)
+
+def on_rx(v):
+    print("RX", v)
+
+p.on_write(on_rx)
+
+i = 0
+while True:
+    if p.is_connected():
+        # Short burst of queued notifications.
+        for _ in range(3):
+            data = str(i) + "_"
+            print("TX", data)
+            p.send(data)
+            i += 1
+    time.sleep(0.1)
