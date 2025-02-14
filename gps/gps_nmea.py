6de96e1a03f790a5e4ea51b6ceb65a07ad4b7cf0
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-02-14 14:57:19 +0900

    Update gps_nmea.py

--- gps/gps_nmea.py
+++ gps/gps_nmea.py
@@ -1,7 +1,7 @@
-from machine import UART
+from machine import Pin, UART
 import time
 
-uart0 = UART(0, baudrate=9600, tx=0, rx=1)
+uart0 = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
 
 if __name__ == '__main__':
     while True:
