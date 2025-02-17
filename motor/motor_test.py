f37e72ad892cc1fcfa6ef4cc54deac7092ef7e19
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-02-17 12:51:40 +0900

    Update motor_test.py

--- motor/motor_test.py
+++ motor/motor_test.py
@@ -15,18 +15,40 @@ STBY = Pin(9, Pin.OUT, value = 1)
 def forward(rate_a, rate_b):
     rate_a = max(0, min(rate_a, 100))
     rate_b = max(0, min(rate_b, 100))
+
     AIN1.off()
     AIN2.on()
-    PWMA.duty_u16(65535 * rate_a // 100)
+    PWMA.duty_u16(int(65535 * rate_a / 100))
     BIN1.on()
     BIN2.off()
-    PWMB.duty_u16(65535 * rate_b // 100)
+    PWMB.duty_u16(int(65535 * rate_b / 100))
 
+def turn_right(rate):
+    rate = max(0, min(rate, 100))
+
+    AIN1.on()
+    AIN2.off()
+    PWMA.duty_u16(int(65535 * rate / 100))
+    BIN1.on()
+    BIN2.off()
+    PWMB.duty_u16(int(65535 * rate / 100))
+    
+def turn_left(rate):
+    rate = max(0, min(rate, 100))
+
+    AIN1.off()
+    AIN2.on()
+    PWMA.duty_u16(int(65535 * rate / 100))
+    BIN1.off()
+    BIN2.on()
+    PWMB.duty_u16(int(65535 * rate / 100))
+
+# 停止
 def stop():
-        AIN1.off()
-        AIN2.off()
-        BIN1.off()
-        BIN2.off()
+    AIN1.off()
+    AIN2.off()
+    BIN1.off()
+    BIN2.off()
     
 if __name__ == '__main__':
     try:
