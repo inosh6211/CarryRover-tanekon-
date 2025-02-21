5de5a273d6c83b1cfb2071510606110544a74334
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-02-21 13:39:29 +0900

    Update Bluetooth_test.py

--- Bluetooth/Bluetooth_test.py
+++ Bluetooth/Bluetooth_test.py
@@ -4,9 +4,12 @@ import time
 
 ble = bluetooth.BLE()
 p = BLESimplePeripheral(ble, name="RPpicoW")
+time.sleep(1)
 
 if __name__ == '__main__':
+    count = 0
     while True:
         if p.is_connected():
-            p.send("Hello World")
+            p.send(str(count))
+            count += 1
             time.sleep(1)
