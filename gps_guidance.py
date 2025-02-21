33de8dbc1ad62c76409cdf7ad9fbfa57fc85902b
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-02-21 12:49:23 +0900

    Update gps_guidance.py

--- gps_guidance.py
+++ gps_guidance.py
@@ -1,18 +1,136 @@
-from machine import Pin, PWM, UART, I2C, Timer
+from machine import Pin, PWM, UART, I2C, SPI, Timer
 from micropyGPS import MicropyGPS
+from ble_simple_peripheral import BLESimplePeripheral
 from bno055 import *
 import math
 import time
+import os
+import bluetooth
+import sdcard
+import _thread
 
 PPR = 3              # PPR = CPR / 4
 GEAR_RATIO = 297.92  # ギア比
 FREQ = 20            # タイマー割り込みの周波数[hz]
-KP_RPM = 0.1         # P制御の比例ゲイン
+KP_RPM = 0.2         # P制御の比例ゲイン
 
 EARTH_RADIUS = 6378137                       # 地球の半径(m)
 GOAL_LAT,GOAL_LON = 35.9186300, 139.9081696  # 7号館
 
+DEVICE_NAME = "RPpicoW"
+FILE_NAME = "CarryRover"
+
+I2C0 = I2C(0, sda=machine.Pin(20), scl=machine.Pin(21))
+SPI1 = SPI(1, sck=Pin(10), mosi=Pin(11), miso=Pin(12))
+SPI1_CS = Pin(13, Pin.OUT)
+UART0 = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
+
+# モータードライバピン設定
+AIN1 = Pin(18, Pin.OUT)
+AIN2 = Pin(17, Pin.OUT)
+PWMA = PWM(Pin(16))
+PWMA.freq(1000)
+BIN1 = Pin(19, Pin.OUT)
+BIN2 = Pin(22, Pin.OUT)
+PWMB = PWM(Pin(28))
+PWMB.freq(1000)
+STBY = Pin(9, Pin.OUT, value=1)
+
+# エンコーダピン設定
+OUTA_A = Pin(3, Pin.IN)
+OUTB_A = Pin(2, Pin.IN)
+OUTA_B = Pin(7, Pin.IN)
+OUTB_B = Pin(6, Pin.IN)
+
+# モーターの状態
+STOP       = 0
+FORWARD    = 1
+TURN_RIGHT = 2
+TURN_LEFT  = 3
+BACKWARD   = 4
+
+
+class Logger:
+    def __init__(self, spi, cs):
+        self.spi = spi
+        self.cs = cs
+        self.sd = None
+        self.file_name = None
+        self.sd_state = False
+        self.ble = BLESimplePeripheral(bluetooth.BLE(), name=DEVICE_NAME)
+        
+        while not self.ble.is_connected():
+            print("Waiting for Bluetooth connection...")
+            time.sleep(1)
+        print("Bluetooth connected")
+        time.sleep(1)
+        
+        try:
+            self.sd = sdcard.SDCard(self.spi, self.cs)
+            os.mount(self.sd, "/sd")
+            self.create_file()
+            self.sd_state = True
+        except Exception as e:
+            print(f"SD card not detected: {e}")
+            if self.ble.is_connected():
+                self.ble.send(f"SD card not detected: {e}")
+        time.sleep(1)
+
+        self.log_queue = []
+        self.log_lock = _thread.allocate_lock()
+        
+        _thread.start_new_thread(self.log_thread, ())
+
+    def create_file(self):
+        counter = 0
+        while True:
+            file_name = f"/sd/{FILE_NAME}{counter}.txt"
+            try:
+                with open(file_name, "x") as f:
+                    print(f"File created: {FILE_NAME}{counter}.txt")
+                    if self.ble.is_connected():
+                        self.ble.send(f"File created: {FILE_NAME}{counter}.txt")
+                    self.file_name = file_name
+                    return file_name
+            except OSError:
+                counter += 1
+
+    def log_thread(self):
+        while True:
+            print(self.log_queue)
+            self.log_lock.acquire()
+            if self.log_queue:
+                message = self.log_queue.pop(0)
+                self.log_lock.release()
+                self.write_log(message)
+            else:
+                self.log_lock.release()
+                time.sleep(0.01)
+
+    def write_log(self, message):
+        print(message)
+        if self.ble.is_connected():
+            self.ble.send(message)
+        if self.sd_state:
+            try:
+                with open(self.file_name, "a") as f:
+                    f.write(message + "\n")
+            except OSError as e:
+                self.sd_state = False
+                print(f"SD card write error: {e}")
+
+    def message(self, message):
+        self.log_lock.acquire()
+        self.log_queue.append(message)
+        self.log_lock.release()
+
+
 class BNO055Handler:
+    """
+    [キャリブレーション]
+    BNO055を完全に静止させる。
+    BNO055を空中で8の字に回転させる
+    """
     def __init__(self, i2c):
         self.i2c = i2c
         self.bno055 = BNO055(self.i2c)
@@ -21,14 +139,17 @@ class BNO055Handler:
         self.yaw = 0
         self.heading = 0
         
-    def get_calibration_status(self):
-        """
-        BNO055を完全に静止させる。
-        BNO055を6方向に向けて静止させる
-        BNO055を空中で8の字に回転させる
-        """
-        self.sys, self.gyro, self.accel, self.mag = self.bno055.cal_status()
-        return self.bno055.calibrated()
+        while True:
+            self.sys, self.gyro, self.accel, self.mag = self.bno055.cal_status()
+            
+            if self.gyro == 3 and self.mag == 3:
+                print("calibration")
+                break
+            
+            else:
+                print(f"gyro:{self.gyro}, mag:{self.mag}")
+                
+            time.sleep(1)
         
     def compute_euler(self):
         self.bno055.iget(QUAT_DATA)
@@ -61,32 +182,20 @@ class BNO055Handler:
         if self.heading < 0:
             self.heading += 360
 
-class Motor:
-    """
-    [モーターの状態:state]
-    STOP = 0
-    FORWARD = 1
-    TURN_RIGHT = 2
-    TURN_LEFT  = 3
-    BACKWARD = 4
-    """
-    def __init__(self, ain1, ain2, pwma, bin1, bin2, pwmb, stby, outa1, outb1, outa2, outb2):
-        # モーター用ピンの設定 (Aが右, Bが左)
-        self.AIN1 = Pin(ain1, Pin.OUT)
-        self.AIN2 = Pin(ain2, Pin.OUT)
-        self.PWMA = PWM(Pin(pwma))
-        self.PWMA.freq(1000)
-        self.BIN1 = Pin(bin1, Pin.OUT)
-        self.BIN2 = Pin(bin2, Pin.OUT)
-        self.PWMB = PWM(Pin(pwmb))
-        self.PWMB.freq(1000)
-        self.STBY = Pin(stby, Pin.OUT, value=1)
 
-        # エンコーダ用ピンの設定
-        self.OUTA_1 = Pin(outa1, Pin.IN)
-        self.OUTB_1 = Pin(outb1, Pin.IN)
-        self.OUTA_2 = Pin(outa2, Pin.IN)
-        self.OUTB_2 = Pin(outb2, Pin.IN)
+class Motor:
+    def __init__(self):
+        self.AIN1 = AIN1
+        self.AIN2 = AIN2
+        self.PWMA = PWMA
+        self.BIN1 = BIN1
+        self.BIN2 = BIN2
+        self.PWMB = PWMB
+        self.STBY = STBY
+        self.OUTA_A = OUTA_A
+        self.OUTB_A = OUTB_A
+        self.OUTA_B = OUTA_B
+        self.OUTB_B = OUTB_B
 
         # モーター制御用変数の設定
         self.rate_a = 0
@@ -100,72 +209,63 @@ class Motor:
         self.start_time = time.ticks_ms()
         self.state = 0
 
-        # エンコーダの割り込み設定
-        self.OUTA_1.irq(trigger=Pin.IRQ_RISING, handler=self.pulse_counter_a)
-        self.OUTA_2.irq(trigger=Pin.IRQ_RISING, handler=self.pulse_counter_b)
-        
+        # エンコーダの割り込み設定（OUTA_A と OUTA_B に設定）
+        self.OUTA_A.irq(trigger=Pin.IRQ_RISING, handler=self.pulse_counter_a)
+        self.OUTA_B.irq(trigger=Pin.IRQ_RISING, handler=self.pulse_counter_b)
+
         # タイマーオブジェクトを生成
         self.timer = Timer()
-    
+
     def pulse_counter_a(self, pin):
-        if self.OUTA_1.value() == self.OUTB_1.value():
+        if self.OUTA_A.value() == self.OUTB_A.value():
             self.direction_a = 1
         else:
             self.direction_a = -1
         self.pulse_count_a += self.direction_a
 
     def pulse_counter_b(self, pin):
-        if self.OUTA_2.value() == self.OUTB_2.value():
+        if self.OUTA_B.value() == self.OUTB_B.value():
             self.direction_b = 1
         else:
             self.direction_b = -1
         self.pulse_count_b += self.direction_b
-
-    def forward(self):
-        if self.state != 1:
+        
+    def run(self, state):
+        if self.state != state:
             self.stop()
+            self.state = state
+
+        if self.state == FORWARD:
+            self.AIN1.off()
+            self.AIN2.on()
+            self.PWMA.duty_u16(int(65535 * self.rate_a / 100))
+            self.BIN1.on()
+            self.BIN2.off()
+            self.PWMB.duty_u16(int(65535 * self.rate_b / 100))
             
-        self.AIN1.off()
-        self.AIN2.on()
-        self.PWMA.duty_u16(int(65535 * self.rate_a / 100))
-        self.BIN1.on()
-        self.BIN2.off()
-        self.PWMB.duty_u16(int(65535 * self.rate_b / 100))
-        self.state = 1
-
-    def turn_right(self):
-        if self.state != 2:
-            self.stop()
+        elif self.state == TURN_RIGHT:
+            self.AIN1.on()
+            self.AIN2.off()
+            self.PWMA.duty_u16(int(65535 * self.rate_a / 100))
+            self.BIN1.on()
+            self.BIN2.off()
+            self.PWMB.duty_u16(int(65535 * self.rate_b / 100))
             
-        self.AIN1.on()
-        self.AIN2.off()
-        self.PWMA.duty_u16(int(65535 * self.rate_a / 100))
-        self.BIN1.on()
-        self.BIN2.off()
-        self.PWMB.duty_u16(int(65535 * self.rate_b / 100))
-        self.state = 2
-
-    def turn_left(self):
-        if self.state != 3:
-            self.stop()
-        self.AIN1.off()
-        self.AIN2.on()
-        self.PWMA.duty_u16(int(65535 * self.rate_a / 100))
-        self.BIN1.off()
-        self.BIN2.on()
-        self.PWMB.duty_u16(int(65535 * self.rate_b / 100))
-        self.state = 3
-
-    def backward(self):
-        if self.state != 4:
-            self.stop()
-        self.AIN1.on()
-        self.AIN2.off()
-        self.PWMA.duty_u16(int(65535 * self.rate_a / 100))
-        self.BIN1.off()
-        self.BIN2.on()
-        self.PWMB.duty_u16(int(65535 * self.rate_b / 100))
-        self.state = 4
+        elif self.state == TURN_LEFT:
+            self.AIN1.off()
+            self.AIN2.on()
+            self.PWMA.duty_u16(int(65535 * self.rate_a / 100))
+            self.BIN1.off()
+            self.BIN2.on()
+            self.PWMB.duty_u16(int(65535 * self.rate_b / 100))
+            
+        elif self.state == BACKWARD:
+            self.AIN1.on()
+            self.AIN2.off()
+            self.PWMA.duty_u16(int(65535 * self.rate_a / 100))
+            self.BIN1.off()
+            self.BIN2.on()
+            self.PWMB.duty_u16(int(65535 * self.rate_b / 100))
 
     def stop(self):
         self.AIN1.off()
@@ -179,38 +279,47 @@ class Motor:
     def compute_rpm(self, pulse_count, interval):
         if interval <= 0:
             return 0
+        
         rpm = abs((pulse_count * 60) / (PPR * GEAR_RATIO * interval))
         return rpm
 
     def update_speed(self, timer):
-        now = time.ticks_ms()
-        interval = (now - self.start_time) / 1000
-        rpm_a = self.compute_rpm(self.pulse_count_a, interval)
-        rpm_b = self.compute_rpm(self.pulse_count_b, interval)
-        self.rate_a += KP_RPM * (self.target_rpm_a - rpm_a)
-        self.rate_b += KP_RPM * (self.target_rpm_b - rpm_b)
-        self.rate_a = min(max(self.rate_a, 0), 100)
-        self.rate_b = min(max(self.rate_b, 0), 100)
-        self.start_time = now
-        self.pulse_count_a = 0
-        self.pulse_count_b = 0
-    
+        if self.state == 0:
+            self.start_time = time.ticks_ms()
+            self.pulse_count_a = 0
+            self.pulse_count_b = 0
+            
+        else:
+            now = time.ticks_ms()
+            interval = (now - self.start_time) / 1000
+            rpm_a = self.compute_rpm(self.pulse_count_a, interval)
+            rpm_b = self.compute_rpm(self.pulse_count_b, interval)
+            self.rate_a += KP_RPM * (self.target_rpm_a - rpm_a)
+            self.rate_b += KP_RPM * (self.target_rpm_b - rpm_b)
+            self.rate_a = min(max(self.rate_a, 0), 100)
+            self.rate_b = min(max(self.rate_b, 0), 100)
+            
+            self.run(self.state)
+            
+            self.start_time = now
+            self.pulse_count_a = 0
+            self.pulse_count_b = 0
+
     def update_rpm(self, target_rpm_a, target_rpm_b):
         self.target_rpm_a = target_rpm_a
         self.target_rpm_b = target_rpm_b
 
-    # 割り込み有効化
     def enable_irq(self):
-        self.OUTA_1.irq(trigger=Pin.IRQ_RISING, handler=self.pulse_counter_a)
-        self.OUTA_2.irq(trigger=Pin.IRQ_RISING, handler=self.pulse_counter_b)
+        self.OUTA_A.irq(trigger=Pin.IRQ_RISING, handler=self.pulse_counter_a)
+        self.OUTA_B.irq(trigger=Pin.IRQ_RISING, handler=self.pulse_counter_b)
         self.timer.init(mode=Timer.PERIODIC, freq=FREQ, callback=self.update_speed)
         self.start_time = time.ticks_ms()
 
-    # 割り込み無効化
     def disable_irq(self):
         self.timer.deinit()
-        self.OUTA_1.irq(handler=None)
-        self.OUTA_2.irq(handler=None)
+        self.OUTA_A.irq(handler=None)
+        self.OUTA_B.irq(handler=None)
+        
 
 class GPS:
     def __init__(self, uart):
@@ -267,51 +376,41 @@ class GPS:
                             return True
                 except IndexError as e:
                     print(f"Error: {e}")
+            
+            else:
+                print("Low accuracy")
+                
         return False
 
 def gps_guidance(goal_lat, goal_lon):
-    motor.update_rpm(30, 30)
     motor.enable_irq()
-    motor_state = 0
+    motor.update_rpm(30, 30)
+    
+    while not gps.update_data(goal_lat, goal_lon):
+        gps.read_nmea()
+        print("Wait GPS signal...")
+        time.sleep(1)
+            
     while True:
         gps.read_nmea()
         if gps.update_data(goal_lat, goal_lon):
             if gps.distance <= 5:
                 motor.stop()
-                print("stoped")
+                print("Stoped")
                 break
             
         bno.compute_heading()
+        bno.compute_euler()
         azimuth_error = ((gps.azimuth - bno.heading + 180) % 360) - 180
-        if azimuth_error > 20:
-            motor.turn_right
-
-        elif azimuth_error < -20:
-            motor.turn_left()
-            
-        else:
-            motor.forward()
-            
-        print(gps.distance, azimuth_error)
-        time.sleep(0.01)
-
-if __name__ == '__main__':
-    try:
-        motor = Motor(
-            ain1=18, ain2=17, pwma=16,
-            bin1=19, bin2=22, pwmb=28,
-            stby=9,
-            outa1=3, outb1=2,
-            outa2=7, outb2=6
-        )
-        uart0 = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
-        gps = GPS(uart0)
-        i2c_0 = I2C(0, sda=machine.Pin(20), scl=machine.Pin(21))
-        bno = BNO055Handler(i2c=i2c_0)
-    
-        gps_guidance(GOAL_LAT, GOAL_LON)
         
-    except KeyboardInterrupt:
-        motor.stop()
-        motor.disable_irq()
-        print("stopped!")
+        if azimuth_error > 20:
+            motor.run(TURN_RIGHT)
+            
+        elif azimuth_error < -20:
+            motor.run(TURN_LEFT)
+        
+        else:
+            motor.run(FORWARD)
+            
+        log.message(f"Distance:{gps.distance}, Azimuth:{azimuth_error}")
+        time.sleep(0.1)
