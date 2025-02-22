687a588ee44156cf24361c16b699eccbd2712ae9
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-02-22 21:31:31 +0900

    Create ControlArm.py

---
+++ arm/ControlArm.py
@@ -0,0 +1,28 @@
+from machine import I2C, Pin
+from servo import Servos
+import utime
+
+I2C1 = I2C(1, scl=Pin(27), sda=Pin(26))
+
+# アームの初期位置
+ARM_INIT_ANGLE0 = 90
+ARM_INIT_ANGLE1 = 180
+ARM_INIT_ANGLE2 = 0
+ARM_INIT_ANGLE3 = 0
+ARM_INIT_ANGLE4 = 10
+
+
+class ControlArm:
+    def __init__(self, i2c):
+        self.servos = Servos(i2c)
+        self.angles = [
+            ARM_INIT_ANGLE0,
+            ARM_INIT_ANGLE1,
+            ARM_INIT_ANGLE2,
+            ARM_INIT_ANGLE3,
+            ARM_INIT_ANGLE4
+        ]
+
+    def control_servo(self, index, angle):
+        self.servos.position(index, degrees=angle)
+        self.angles[index] = angle 
