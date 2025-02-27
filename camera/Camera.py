802e8fa8eabe28e81348cbf303af9d0e0911c2ef
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-02-27 21:33:27 +0900

    Update Camera.py

--- camera/Camera.py
+++ camera/Camera.py
@@ -2,35 +2,26 @@ from machine import Pin, UART
 import time
 import math
 
-#UARTの初期設定（Pico W側、GPIO4=TX, GPIO5=RX）
+# UARTの初期設定（Pico W側、GPIO4=TX, GPIO5=RX）
 UART1 = UART(1, baudrate=115200, tx=Pin(4), rx=Pin(5))
 
-#キャリブレーション結果
+# キャリブレーション結果
 TAG_SIZE = 30
 FOCAL_LENGTH_X = 210.70  
 FOCAL_LENGTH_Y = 210.70
 
-CENTER_X = 100 # ROI_W / 2
-CENTER_Y = 100 # ROI_H / 2
+CENTER_X = 100  # ROI_W / 2
+CENTER_Y = 100  # ROI_H / 2
 DIST_COEFFS = [-0.34155103, 0.25499062, -0.00517389, -0.00731524, -0.17990041]
 
 FOCAL_LENGTH_PX = 210.70
 TAG_SIZE = 30
 
-
 class Camera:
-    def __init__(self,uart):
+    def __init__(self, uart):
         self.uart = uart
-        self.color_pixels = []
-        self.color_cx = []
-        self.color_cy = []
-        self.tag_detected = []
-        self.tag_cx = []
-        self.tag_cy = []
-        self.tag_distance = []
-        self.tag_pitch = []
             
-    #歪み補正
+    # 歪み補正
     def undistort_point(self, x, y):
         norm_x = (x - CENTER_X) / FOCAL_LENGTH_X
         norm_y = (y - CENTER_Y) / FOCAL_LENGTH_Y
@@ -43,49 +34,64 @@ class Camera:
         return corrected_x, corrected_y
 
     def read_camera(self):
+        message = ""
         while not self.uart.any():
             time.sleep(0.01)
                 
-        while uart1.any() > 0:
+        while self.uart.any() > 0: 
             message += self.uart.read().decode('utf-8').strip()
             
         data = message.split(',')
-        
         return data
 
     def read_color(self):
-        self.uart.write("0\n")
+        self.uart.write("c\n")
         data = self.read_camera()
         if data[0] == "c":
+            self.color_pixels = []
+            self.color_cx = []
+            self.color_cy = []
+
             for color in range(3):
-                self.color_pixels = float(data[color + 1])
-                cx, cy = float(data[color + 2]), (data[color + 3])
-                self.color_cx, self.color_cy = undistort_point(cx, cy)
+                self.color_pixels.append(float(data[color*4 + 2]))
+                cx, cy = float(data[color*4 + 3]), float(data[color*4 + 4])
+                #undistorted_cx, undistorted_cy = self.undistort_point(cx, cy)
+                self.color_cx.append(cx)
+                self.color_cy.append(cy)
 
     def read_tag(self):
-        self.uart.write("1\n")    
-        data = read_camera()
+        self.uart.write("t\n")    
+        data = self.read_camera()
         if data[0] == "t":
+            self.tag_detected = []
+            self.tag_cx = []
+            self.tag_cy = []
+            self.tag_distance = []
+            self.tag_pitch = []
+
             for tag_id in range(10):
-                self.tag_detected = int(data[tag_id + 1])
+                self.tag_detected.append(int(data[tag_id + 1]))
                 cx, cy = float(data[tag_id + 2]), float(data[tag_id + 3])
-                self.tag_cx, self.tag_cy = undistort_point(cx, cy)
-                self.tag_distance = float(data[tag_id + 4])
-                self.tag_pitch = float(data[tag_id + 5])
-        
+                undistorted_cx, undistorted_cy = self.undistort_point(cx, cy)
+                self.tag_cx.append(undistorted_cx)
+                self.tag_cy.append(undistorted_cy)
+                self.tag_distance.append(float(data[tag_id + 4]))
+                self.tag_pitch.append(float(data[tag_id + 5]))
 
 if __name__ == "__main__":
     camera = Camera(UART1)
+    time.sleep(5)
     
     while True:
         camera.read_color()
-        camra.read_tag()
         for i in range(3):
             if camera.color_pixels:
-                print(camera.color_pixels = [i], camera.color_cx = [i], self.color_cy = [i])
-            
+                print(i, camera.color_pixels[i], camera.color_cx[i], camera.color_cy[i])
+        """
+        camera.read_tag()    
         for j in range(10):
             if camera.tag_detected:
-                print(camera.tag_cx = [j], camera.tag_cy = [j], camera.tag_distance = [j], camera.tag_pitch = [j])
-        
+                print(camera.tag_cx[j], camera.tag_cy[j], camera.tag_distance[j], camera.tag_pitch[j])
+        """
         time.sleep(0.1)
+
