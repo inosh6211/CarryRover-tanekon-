109d672e31be801b164cafe008bb6f5304d43757
Rintaro-Suzuki-TUS <7324065@ed.tus.ac.jp>
2025-02-28 18:01:25 +0900

    Create unitv_send_Apriltag.py

---
+++ unitv/unitv_send_Apriltag.py
@@ -0,0 +1,55 @@
+import sensor
+import image
+import time
+import math
+from fpioa_manager import fm
+from machine import UART
+
+#UART設定
+fm.register(34, fm.fpioa.UART1_TX, force=True)
+fm.register(35, fm.fpioa.UART1_RX, force=True)
+uart = UART(UART.UART1, baudrate=115200, read_buf_len=1024)
+
+#カメラ初期化
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.set_auto_gain(False)        # ゲイン
+sensor.set_auto_whitebal(False)    # ホワイトバランス#もとはFalse
+sensor.set_auto_exposure(False)    # 露光調整
+sensor.set_brightness(False)       # 輝度
+sensor.set_contrast(False)         # コントラスト
+sensor.set_saturation(False)       # 彩度
+
+#ROI設定
+ROI_X, ROI_Y, ROI_W, ROI_H = 0, 50, 240, 260
+FOCAL_LENGTH_PX = 210.70
+TAG_SIZE = 30
+
+
+
+#Apriltag認識
+while True:
+    
+    data_t = [0] * 60
+    img = sensor.snapshot()
+    img.replace(vflip=True, hmirror=False, transpose=True)
+    tags = img.find_apriltags(families=image.TAG16H5,roi=(ROI_X, ROI_Y, ROI_W, ROI_H))
+    if tags:
+        for tag_id in range(10):
+            for tag in tags:
+                if tag_id == tag.id():
+                    data_t[6 * tag_id] = tag_id
+                    data_t[6 * tag.id() + 1] = 1
+                    data_t[6 * tag.id() + 2] = tag.cx() - (ROI_W / 2)
+                    data_t[6 * tag.id() + 3] = tag.cy() - (ROI_H / 2)
+                    data_t[6 * tag.id() + 4] = tag.z_translation()
+                    data_t[6 * tag.id() + 5] = tag.y_rotation()
+                    
+                else:
+                    data_t[6 * tag_id] = tag_id
+        message = ",".join(map(str, data_t))
+        uart.write("t," + message + "\n")
+        print("t," + message + "\n")
+
+    time.sleep(0.1)
