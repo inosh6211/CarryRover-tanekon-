1eafc382c06b9cffe3a758e86a61110935202b81
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-02-28 18:45:35 +0900

    Update Camera.py

--- camera/Camera.py
+++ camera/Camera.py
@@ -42,41 +42,41 @@ class Camera:
             message += self.uart.read().decode('utf-8').strip()
             
         data = message.split(',')
+        
         return data
 
     def read_color(self):
         self.uart.write("c\n")
         data = self.read_camera()
+        
         if data[0] == "c":
             self.color_pixels = []
             self.color_cx = []
             self.color_cy = []
-
+            
             for color in range(3):
-                self.color_pixels.append(float(data[color*4 + 2]))
-                cx, cy = float(data[color*4 + 3]), float(data[color*4 + 4])
-                #undistorted_cx, undistorted_cy = self.undistort_point(cx, cy)
-                self.color_cx.append(cx)
-                self.color_cy.append(cy)
+                self.color_pixels.append(float(data[color*3 + 1]))
+                self.color_cx.append(float(data[color*3 + 2]))
+                self.color_cy.append(float(data[color*3 + 3]))
 
     def read_tag(self):
         self.uart.write("t\n")    
         data = self.read_camera()
+        
         if data[0] == "t":
             self.tag_detected = []
             self.tag_cx = []
             self.tag_cy = []
             self.tag_distance = []
             self.tag_pitch = []
-
+            
             for tag_id in range(10):
-                self.tag_detected.append(int(data[tag_id + 1]))
-                cx, cy = float(data[tag_id + 2]), float(data[tag_id + 3])
-                undistorted_cx, undistorted_cy = self.undistort_point(cx, cy)
-                self.tag_cx.append(undistorted_cx)
-                self.tag_cy.append(undistorted_cy)
-                self.tag_distance.append(float(data[tag_id + 4]))
-                self.tag_pitch.append(float(data[tag_id + 5]))
+                self.tag_detected.append(int(data[tag_id*5 + 1]))
+                self.tag_cx.append(float(data[tag_id*5 + 2]))
+                self.tag_cy.append(float(data[tag_id*5 + 3]))
+                self.tag_distance.append(float(data[tag_id*5 + 4]))
+                self.tag_pitch.append(float(data[tag_id*5 + 5]))
+
 
 if __name__ == "__main__":
     camera = Camera(UART1)
@@ -85,13 +85,13 @@ if __name__ == "__main__":
     while True:
         camera.read_color()
         for i in range(3):
-            if camera.color_pixels:
-                print(i, camera.color_pixels[i], camera.color_cx[i], camera.color_cy[i])
-        """
+            if camera.color_pixels > 0:
+                print(camera.color_pixels[i], camera.color_cx[i], camera.color_cy[i])
+
         camera.read_tag()    
         for j in range(10):
-            if camera.tag_detected:
+            if camera.tag_detected[j] == 1:
                 print(camera.tag_cx[j], camera.tag_cy[j], camera.tag_distance[j], camera.tag_pitch[j])
-        """
+
         time.sleep(0.1)
 
