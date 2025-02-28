08d2a1354f9d1159d865ff1e9c6845b54589d495
Rintaro-Suzuki-TUS <7324065@ed.tus.ac.jp>
2025-02-28 18:24:57 +0900

    Update unitv_send_Apriltag.py

--- unitv/unitv_send_Apriltag.py
+++ unitv/unitv_send_Apriltag.py
@@ -30,26 +30,27 @@ TAG_SIZE = 30
 
 #Apriltag認識
 while True:
-    
-    data_t = [0] * 60
     img = sensor.snapshot()
     img.replace(vflip=True, hmirror=False, transpose=True)
-    tags = img.find_apriltags(families=image.TAG16H5,roi=(ROI_X, ROI_Y, ROI_W, ROI_H))
-    if tags:
-        for tag_id in range(10):
-            for tag in tags:
-                if tag_id == tag.id():
-                    data_t[6 * tag_id] = tag_id
-                    data_t[6 * tag.id() + 1] = 1
-                    data_t[6 * tag.id() + 2] = tag.cx() - (ROI_W / 2)
-                    data_t[6 * tag.id() + 3] = tag.cy() - (ROI_H / 2)
-                    data_t[6 * tag.id() + 4] = tag.z_translation()
-                    data_t[6 * tag.id() + 5] = tag.y_rotation()
-                    
-                else:
-                    data_t[6 * tag_id] = tag_id
-        message = ",".join(map(str, data_t))
-        uart.write("t," + message + "\n")
-        print("t," + message + "\n")
-
+    
+    while uart.any():
+        data = uart.read().decode('utf-8').strip()
+        if data == "t":
+            data_t = [0] * 50
+            img = sensor.snapshot()
+            img.replace(vflip=True, hmirror=False, transpose=True)
+            tags = img.find_apriltags(families=image.TAG16H5,roi=(ROI_X, ROI_Y, ROI_W, ROI_H))
+            if tags:
+                for tag_id in range(10):
+                    for tag in tags:
+                        if tag_id == tag.id():
+                            data_t[5 * tag.id() + 0] = 1
+                            data_t[5 * tag.id() + 1] = tag.cx() - (ROI_W / 2)
+                            data_t[5 * tag.id() + 2] = tag.cy() - (ROI_H / 2)
+                            data_t[5 * tag.id() + 3] = tag.z_translation()
+                            data_t[5 * tag.id() + 4] = tag.y_rotation()
+                message = ",".join(map(str, data_t))
+                uart.write("t," + message + "\n")
+                print("t," + message + "\n")
+    
     time.sleep(0.1)
