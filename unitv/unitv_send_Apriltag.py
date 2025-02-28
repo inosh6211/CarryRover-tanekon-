68c09c77993d4a68a7095ebd59ce9c4180bbe4d8
Rintaro-Suzuki-TUS <7324065@ed.tus.ac.jp>
2025-02-28 19:20:56 +0900

    Update unitv_send_Apriltag.py

--- unitv/unitv_send_Apriltag.py
+++ unitv/unitv_send_Apriltag.py
@@ -41,14 +41,17 @@ while True:
             img.replace(vflip=True, hmirror=False, transpose=True)
             tags = img.find_apriltags(families=image.TAG16H5,roi=(ROI_X, ROI_Y, ROI_W, ROI_H))
             if tags:
-                for tag_id in range(10):
-                    for tag in tags:
-                        if tag_id == tag.id():
-                            data_t[5 * tag.id() + 0] = 1
-                            data_t[5 * tag.id() + 1] = tag.cx() - (ROI_W / 2)
-                            data_t[5 * tag.id() + 2] = tag.cy() - (ROI_H / 2)
-                            data_t[5 * tag.id() + 3] = tag.z_translation()
-                            data_t[5 * tag.id() + 4] = tag.y_rotation()
+                for tag in tags:
+                        data_t[5 * tag.id() + 0] = 1
+                        data_t[5 * tag.id() + 1] = tag.cx() - (ROI_W / 2)
+                        data_t[5 * tag.id() + 2] = tag.cy() - (ROI_H / 2)
+                        data_t[5 * tag.id() + 3] = tag.z_translation()
+                        data_t[5 * tag.id() + 4] = tag.y_rotation()
+            message = ",".join(map(str, data_t))
+            uart.write("t," + message + "\n")
+            print("t," + message + "\n")
+    
+    time.sleep(0.01)
             message = ",".join(map(str, data_t))
             uart.write("t," + message + "\n")
             print("t," + message + "\n")
