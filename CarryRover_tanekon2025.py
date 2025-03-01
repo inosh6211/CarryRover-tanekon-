979cadf88d2230d48a5bb5a19adc1fb3e536510e
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2025-03-01 10:44:35 +0900

    Update CarryRover_tanekon2025.py

--- CarryRover_tanekon2025.py
+++ CarryRover_tanekon2025.py
@@ -392,13 +392,59 @@ class GPS:
                 log.ble_print(f"PDOP: {self.micropygps.pdop}, Satellites: {self.micropygps.satellites_in_use}")
                 
         return False
+    
 
+class CameraReceiver:
+    def __init__(self, uart):
+        self.uart = uart
 
-class ControlArm:
-    """アームのクラスを作る"""
+    def read_camera(self):
+        message = ""
+        while not self.uart.any():
+            time.sleep(0.01)
+                
+        while self.uart.any() > 0: 
+            message += self.uart.read().decode('utf-8').strip()
+            
+        data = message.split(',')
+        
+        return data
 
-def read_camera():
-    """カメラからのデータ取得の関数を作る"""
+    def read_color(self):
+        self.uart.write("c\n")
+        data = self.read_camera()
+        
+        if data[0] == "c":
+            self.color_pixels = []
+            self.color_cx = []
+            self.color_cy = []
+            
+            for color in range(3):
+                self.color_pixels.append(float(data[color*3 + 1]))
+                self.color_cx.append(float(data[color*3 + 2]))
+                self.color_cy.append(float(data[color*3 + 3]))
+
+    def read_tag(self):
+        self.uart.write("t\n")    
+        data = self.read_camera()
+        
+        if data[0] == "t":
+            self.tag_detected = []
+            self.tag_cx = []
+            self.tag_cy = []
+            self.tag_distance = []
+            self.tag_pitch = []
+            
+            for tag_id in range(10):
+                self.tag_detected.append(int(data[tag_id*5 + 1]))
+                self.tag_cx.append(float(data[tag_id*5 + 2]))
+                self.tag_cy.append(float(data[tag_id*5 + 3]))
+                self.tag_distance.append(float(data[tag_id*5 + 4]))
+                self.tag_pitch.append(float(data[tag_id*5 + 5]))
+
+class ArmController:
+    """アーム制御のクラスを作る"""
+    
     
 def start():
     init_pressure = bme.pressure()
@@ -526,6 +572,7 @@ if __name__ == "__main__":
     bme = BME280(i2c=I2C0)
     motor = Motor()
     gps = GPS(UART0)
+    camera = CameraReceiver(UART1)
     
     log.sd_write("Setup completed")
     
