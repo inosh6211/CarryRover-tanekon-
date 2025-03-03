0225e5667ed039786d94cb9c4ddeb2cf9cede391
Rintaro-Suzuki-TUS <7324065@ed.tus.ac.jp>
2025-03-03 18:44:36 +0900

    Update CarryRover_tanekon2025.py

--- CarryRover_tanekon2025.py
+++ CarryRover_tanekon2025.py
@@ -680,14 +680,16 @@ def color_guidance(index):
 
 def apriltag_guidance(index):
      """エイプリルタグでの誘導"""
-    motor = Motor()
-    motor.enable_irq() 
+    #ここから90度回転して正面に来るプログラム
+    KP_YAW=0.3#ここで比例定数を決める
+    #motor = Motor()
+    Motor.enable_irq() 
 
     m=160#ここでapriltagの何ｍｍ前に来るかを決める
 
 
     if __name__ == "__main__":
-        camera = CameraReceiver(UART1)
+        #camera = CameraReceiver(UART1)
         time.sleep(3)
         
         
@@ -699,20 +701,20 @@ def apriltag_guidance(index):
         yaw = 0       # ヨー角
     
            
-        motor.enable_irq()
-        motor.update_rpm(30, 30)
+        Motor.enable_irq()
+        Motor.update_rpm(30, 30)
         
         
         
         print("go")
         
     try:
-        motor = Motor()
+        #motor = Motor()
         motor.enable_irq()    
-        cam = CameraReceiver(UART1)
+        #cam = CameraReceiver(UART1)
         
         while True:
-            cam.read_tags(0)
+            CameraReceiver(UART1).read_tags(0)
             print(cam.tag_detected[6])
             time.sleep(0.1)
             
@@ -721,14 +723,14 @@ def apriltag_guidance(index):
             
             if cam.tag_detected[0]:
                 print("0")
-                roll, pitch, yaw = euler()
+                roll, pitch, yaw = compute_euler()
                 init_yaw = yaw
                 #forward_to_Apriltag_mm(m)
                 print(f"complete")
                
             elif cam.tag_detected[1]:
                 print(f"1")
-                roll, pitch, yaw = euler()
+                roll, pitch, yaw = compute_euler()
                 init_yaw = yaw
                 soutai_turn_right_90()
                 straight_forward_t(2)
@@ -740,7 +742,7 @@ def apriltag_guidance(index):
                  
             elif cam.tag_detected[2]:#ここで特異点を超えてしまうやつが発生する危険性あり
                 print(f"2")
-                roll, pitch, yaw = euler()
+                roll, pitch, yaw = compute_euler()
                 init_yaw = yaw
                 soutai_turn_right_90()
                 straight_forward_t(2)
@@ -753,7 +755,7 @@ def apriltag_guidance(index):
                 
             elif cam.tag_detected[3]:
                 print(f"3")
-                roll, pitch, yaw = euler()
+                roll, pitch, yaw = compute_euler()
                 init_yaw = yaw;
                 soutai_turn_left_90()
                 straight_forward_t(2)
@@ -761,7 +763,7 @@ def apriltag_guidance(index):
                 straight_forward_t(2)
                 soutai_turn_right_90()
                 print(f"complete")
-        """エイプリルタグでの誘導"""
+        #ここまで90度回転して正面に来るプログラム
         
         
     def collect_material():
