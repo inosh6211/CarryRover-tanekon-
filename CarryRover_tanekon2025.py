a4ad524fe1880d8a86549945cb0119c3e18ef5de
Rintaro-Suzuki-TUS <7324065@ed.tus.ac.jp>
2025-03-07 17:46:54 +0900

    Update CarryRover_tanekon2025.py

--- CarryRover_tanekon2025.py
+++ CarryRover_tanekon2025.py
@@ -323,27 +323,44 @@ class Motor:
                 break
             time.sleep(0.01)
             
-    def straight_forward_t(self, distance, rpm):
+     def straight_ward(self, ward, distance, rpm):
         start=time.ticks_ms()
         t = distance / (135 * math.pi * (rpm /60))
         bno.compute_euler()
         init_yaw = (-bno.yaw + 360) % 360
-            
-        while True:
-            bno.compute_euler()
-            current_yaw = (-bno.yaw + 360) % 360
-            diff = ((current_yaw - init_yaw + 540) % 360) - 180
-            rate_a=-KP_YAW*diff+30
-            rate_b=KP_YAW*diff+30
-            self.update_rpm(rate_a, rate_b)
-            self.run(FORWARD)
-            print(f"L{diff}")
-            now = time.ticks_ms()
-            if (time.ticks_ms() - start) / 1000 >= t:
-                motor.stop()
-                break
-            time.sleep(0.01)
-
+                
+        if ward == f
+            while True:
+                bno.compute_euler()
+                current_yaw = (-bno.yaw + 360) % 360
+                diff = ((current_yaw - init_yaw + 540) % 360) - 180
+                rate_a=-KP_YAW*diff+rpm
+                rate_b=KP_YAW*diff+rpm
+                self.update_rpm(rate_a, rate_b)
+                self.run(FORWARD)
+                print(f"L{diff}")
+                now = time.ticks_ms()
+                if (time.ticks_ms() - start) / 1000 >= t:
+                    motor.stop()
+                    break
+                time.sleep(0.01)
+                
+        elif ward == b
+            while True:
+                bno.compute_euler()
+                current_yaw = (-bno.yaw + 360) % 360
+                diff = ((current_yaw - init_yaw + 540) % 360) - 180
+                rate_a=KP_YAW*diff+rpm
+                rate_b=-KP_YAW*diff+rpm
+                self.update_rpm(rate_a, rate_b)
+                self.run(BACKWARD)
+                print(f"L{diff}")
+                now = time.ticks_ms()
+                if (time.ticks_ms() - start) / 1000 >= t:
+                    motor.stop()
+                    break
+                time.sleep(0.01)
+    
 
     def compute_rpm(self, pulse_count, interval):
         if interval <= 0:
